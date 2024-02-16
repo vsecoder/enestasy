b9401177187b4525735855c331dcc4cac74b2e3e
Всеволод <kreepmeister@gmail.com>
2024-02-16 21:49:11 +0300

    Update avatar and gradient

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
 
   return (
     <ChakraProvider theme={theme}>
-      <Box fontSize="xl" bgGradient='linear-gradient(rgba(34,193,195,0.8505602924763656) 0%, rgba(253,187,45,0.5116247182466737) 30%, transparent 40%)'>
+      <Box fontSize="xl" bgGradient='linear-gradient(180deg, #43e97b 15%, #38f9d7 30%, rgba(255,255,255,0) 59%)'>
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
@@ -37,13 +37,13 @@ function App() {
                 <Code fontSize="xl">IT business</Code> <Code fontSize="xl">Manager</Code>
               </Text>
               <Text>
-              {month === 10 && day === 21 ?
-                (
-                <Link href="https://t.me/enestasy7" color="teal.500" isExternal>А ты уже поздравил меня с ДР?</Link>
-                ) : (
-                <Link href="https://t.me/enestasy7" color="teal.500" isExternal>Telegram</Link>
-                )
-              }
+                {month === 10 && day === 21 ?
+                  (
+                    <Link href="https://t.me/enestasy7" color="teal.500" isExternal>А ты уже поздравил меня с ДР?</Link>
+                  ) : (
+                    <Link href="https://t.me/enestasy7" color="teal.500" isExternal>Telegram</Link>
+                  )
+                }
               </Text>
             </Stack>
           </VStack>
